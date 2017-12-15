02208ab0eb94741d4b18abb477ac6861438d4ecc
jabm0010 <jabm0010@red.ujaen.es>
2017-12-15 19:08:58 +0100

    Update cgvInterface.cpp
    Principio de menu para dificultad

--- cgvInterface.cpp
+++ cgvInterface.cpp
@@ -41,6 +41,8 @@ void cgvInterface::configure_environment(int argc, char** argv,
   glutInitWindowPosition(_pos_X,_pos_Y);
 	glutCreateWindow(_title.c_str());
 
+	create_menu();
+
 	glEnable(GL_DEPTH_TEST); // enable the removal of hidden surfaces by using the z-buffer
 	//Modified to put the colour black
   glClearColor(0,0,0,0.0); // define the background color of the window
@@ -56,6 +58,35 @@ void cgvInterface::configure_environment(int argc, char** argv,
 void cgvInterface::init_rendering_loop() {
 	glutMainLoop(); // initialize the visualization loop of OpenGL
 }
+
+void cgvInterface::create_menu() {
+	int menu_id = glutCreateMenu(menuHandle);
+
+	glutAddMenuEntry("Easy", 1);
+	glutAddMenuEntry("Medium", 2);
+	glutAddMenuEntry("Difficult",3);
+
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+
+}
+void cgvInterface::menuHandle(int value)
+{
+
+;	switch (value) {
+	case 1:
+		difficulty = 0.002;
+		break;
+	case 2:
+		difficulty = 0.004;
+		break;
+	case 3:
+		difficulty = 0.006;
+}
+	glutPostRedisplay(); // renew the content of the window
+}
+
+
 
 void cgvInterface::set_glutKeyboardFunc(unsigned char key, int x, int y) {
   switch (key) {
@@ -106,98 +137,45 @@ void cgvInterface::set_glutDisplayFunc() {
 	// set up the viewport
 	glViewport(0, 0, interface.get_width_window(), interface.get_height_window());
 
-	// Section D: check the mode before applying the camera and projection transformations,
-	if (interface.mode == CGV_SELECT) {
-	  // Section D: if it is in the OpenGL selection mode pass the corresponding parameters to the camera
-		interface.init_selection(1024,impact_list);
-	}
 	// Apply the camera and projection transformations according to its parameters and to the mode (selection or visualization)
 	interface.camera.apply(interface.camType);
 
 	// Render the scene
 	interface.scene.render();
 
-	if (interface.mode == CGV_SELECT) {
-		// Section D: exit the selection mode and process the list of impacts
-		interface.finish_selection(1024,impact_list); 
-		glutPostRedisplay();
-	}	else {
-		// refresh the window
-		glutSwapBuffers(); // it is used instead of glFlush(), to avoid flickering
-	}	
-}
-
-
-void cgvInterface::set_glutMouseFunc(GLint button,GLint state,GLint x,GLint y) {
-
-// TODO: Section D: check if the left button of the mouse has been clicked. See glutMouseFunc for details.  
-
-// Section D: Store the button that has been pressed or released. If it has been clicked, then change to selection mode (CGV_SELECT)
-
-// Section D: Save the position of the pixel when the mouse was clicked
-
-
-	glutPostRedisplay();
-
-
-}
-
-
-void cgvInterface::set_glutMotionFunc(GLint x,GLint y) {
-
-// TODO: Section E: if the mouse button is pressed and there is a selected object, then check the selected object 
-// and the position of the mouse to update the corresponding degree of freedom of the model accordingly 
-
-// Section E: Save the new position of the mouse
-
-// Section E: Redraw the content of the display window 
-
+	glutSwapBuffers(); // it is used instead of glFlush(), to avoid flickering	
 }
 
 void cgvInterface::init_callbacks() {
 	glutKeyboardFunc(set_glutKeyboardFunc);
 	glutReshapeFunc(set_glutReshapeFunc);
-	glutDisplayFunc(set_glutDisplayFunc);
-
-	glutMouseFunc(set_glutMouseFunc); 
-	glutMotionFunc(set_glutMotionFunc); 	
+	glutDisplayFunc(set_glutDisplayFunc); 
+	glutVisibilityFunc(interface.visible);
 }
 
-void cgvInterface::init_selection(int SIZE_IMPACT_LIST, GLuint *impact_list) {
-	// TODO: Section D: select the variable where the impacts will be stored. Use glSelectBuffer
-	// Section D: Change to the OpenGL selection mode. See function glRenderMode
-	// Section D: set the camera to selection mode with the required parameters to apply it
-	// including the height and width of the selection area. 
-	// Prove several alternatives to test the right size of this small area.  
+void cgvInterface::visible(int iVis)
+{
+	if (iVis == GLUT_VISIBLE)
+		glutIdleFunc(interface.idle);
+	else
+		glutIdleFunc(NULL);
 
-
-}
-int process_impacts(int num_impacts, GLuint *impact_list) {
-/* TODO: Section D: this function should return the code of the selected object, that can be different of the name assigned in the stack of names, 
-  If hierarchical names have used it has to be taken into account in this function to return a single code */
-
-	// Section D: traverse the list of impacts with num_impacts impacts,
-	// save the nearer to the observer (minimum Z)
-	// At the beginning consider that minimum Z has a value of 0xffffffff (the maximum value represented by type GLuint)
-
-
-	// Section D: From the information of the impact with a minimum Z, return the code corresponding to this object: as the scene 
-	// is not saved in any data structure, to return the selected object use the names directly assigned to the objects 
-
-	return -1; 
+	return;
 }
 
-void cgvInterface::finish_selection(int SIZE_IMPACT_LIST, GLuint *impact_list) {
+void cgvInterface::idle(void)
+{
+	float fTime, fSimTime;
+	float static fLastIdleTime = 0;
 
-	// TODO 
-	// Section D: Change to the OpenGL visualization mode and get the number of impacts  
+	fTime = 1000 / 60;
 
-	// Section D: if there are impacts, process them with the function process_impacts(int num_impacts, GLuint *impact_list);
-	// obtaining the selected object, if any
+	fSimTime = fTime - fLastIdleTime;
 
-	// Section D: the selection has finished, change to normal visualization
+	interface.scene.ballMovement();
 
-	// Section D: set up the camera with the parameters of the visualization mode
+	fLastIdleTime = fTime;
 
+	glutPostRedisplay();
 }
 
