6666cd28300f26b68b77a17da88c4f64dda62358
jabm0010 <jabm0010@red.ujaen.es>
2017-12-15 19:09:39 +0100

    Update cgvInterface.h

--- cgvInterface.h
+++ cgvInterface.h
@@ -1,9 +1,3 @@
-#ifndef __CGVINTERFACE
-#define __CGVINTERFACE
-
-#if defined(__APPLE__) && defined(__MACH__)
-#include <GLUT/glut.h>
-#include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
 #include <GL/glut.h>
@@ -28,6 +22,10 @@ class cgvInterface {
 		cgvCamera camera; // Camera to visualize the scene
 		cameraType camType; // Camera type 	CGV_PARALLEL or CGV_PERSPECTIVE
 
+		double difficulty;
+
+
+
 		///// Section D: attributes to selecting by mouse
 		interfaceMode mode; // CGV_VISUALIZE: in the window where the scene is usually rendered  
 							// CGV_SELECT: the user has clicked, the scene must be rendered in selection mode to compute the list of 							  // impacts
@@ -47,16 +45,14 @@ class cgvInterface {
 		                                               // it is automatically called when the window is resized
 		static void set_glutDisplayFunc(); // method to render the scene
 
-	///// Section D: methods to control click and drag with the mouse
-		static void  set_glutMouseFunc(GLint button,GLint state,GLint x,GLint y); // control mouse clicking
-		static void  set_glutMotionFunc(GLint x,GLint y); // control the mouse movement while a button is pressed
+		static void  set_idleFunc();
 
-		
-		// Methods
-		///// Section D: methods with OpenGL functions for selecting objects by list of impacts
-		void init_selection(int SIZE_IMPACT_LIST, GLuint *impact_list);
-		void finish_selection(int SIZE_IMPACT_LIST, GLuint *impact_list);
 
+
+
+
+		static void menuHandle(int value); // method to handle the menu
+		void create_menu(); // Create a menu that is handle with the right button of the mouse. 
 		
 		// create the world that is render in the window
 		void create_world(void);
@@ -69,6 +65,10 @@ class cgvInterface {
 		void init_callbacks(); // initialize all the callbacks
 
 		void init_rendering_loop(); // render the scene and wait for an event in the interface
+
+		static void visible(int vis);
+		static void idle();
+		void init_idleFunc();
 
 		// methods get_ and set_ to access the attributes
 		int get_width_window(){return width_window;};
