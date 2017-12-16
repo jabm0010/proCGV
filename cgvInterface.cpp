bf01fa1959c83057dfbb8c6040e91197070f77a4
jabm0010 <jabm0010@red.ujaen.es>
2017-12-16 18:08:49 +0100

    Version 1.0

--- cgvInterface.cpp
+++ cgvInterface.cpp
@@ -5,55 +5,52 @@
 
 extern cgvInterface interface; // the callbacks must be static and this object is required to access to the variables of the class                   // ellos a las variables de la clase
 
-// Constructor and destructor methods -----------------------------------
+							   // Constructor and destructor methods -----------------------------------
 
-cgvInterface::cgvInterface():camType(CGV_PARALLEL)  {
-//// Section D: initialize the attributes to select objects by list of impacts
+cgvInterface::cgvInterface() :camType(CGV_PARALLEL) {
+	//// Section D: initialize the attributes to select objects by list of impacts
 	mode = CGV_VISUALIZE;
-	selected_object = -1;
-	pressed_button = false;
-
 }
 
-cgvInterface::~cgvInterface () {}
+cgvInterface::~cgvInterface() {}
 
 
 // Public methods ----------------------------------------
 void cgvInterface::create_world(void) {
 	//Changed the camera view to the top
-	camera = cgvCamera(cgvPoint3D(0,8.0,0),cgvPoint3D(0,0,0),cgvPoint3D(0,0,1),
-		                                1*5, 1*5, 0.1, 200);
+	camera = cgvCamera(cgvPoint3D(0, 8.0, 0), cgvPoint3D(0, 0, 0), cgvPoint3D(0, 0, 1),
+		1 * 5, 1 * 5, 0.1, 200);
 
 }
 
-void cgvInterface::configure_environment(int argc, char** argv, 
-			                       int _width_window, int _height_window, 
-			                       int _pos_X, int _pos_Y, 
-													 string _title)
-													 {
+void cgvInterface::configure_environment(int argc, char** argv,
+	int _width_window, int _height_window,
+	int _pos_X, int _pos_Y,
+	string _title)
+{
 	// initialization of the interface variables																	
 	width_window = _width_window;
 	height_window = _height_window;
 
 	// initialization of the display window
 	glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowSize(_width_window,_height_window);
-  glutInitWindowPosition(_pos_X,_pos_Y);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(_width_window, _height_window);
+	glutInitWindowPosition(_pos_X, _pos_Y);
 	glutCreateWindow(_title.c_str());
 
 	create_menu();
 
 	glEnable(GL_DEPTH_TEST); // enable the removal of hidden surfaces by using the z-buffer
-	//Modified to put the colour black
-  glClearColor(0,0,0,0.0); // define the background color of the window
+							 //Modified to put the colour black
+	glClearColor(0, 0, 0, 0.0); // define the background color of the window
 
 	glInitNames();
 
 	glEnable(GL_LIGHTING); // enable the lighting of the scene
-  glEnable(GL_NORMALIZE); // normalize the normal vectors required by the lighting computation. 
+	glEnable(GL_NORMALIZE); // normalize the normal vectors required by the lighting computation. 
 
-  create_world(); // create the world (scene) to be rendered in the window
+	create_world(); // create the world (scene) to be rendered in the window
 }
 
 void cgvInterface::init_rendering_loop() {
@@ -65,16 +62,16 @@ void cgvInterface::create_menu() {
 
 	glutAddMenuEntry("Easy", 1);
 	glutAddMenuEntry("Medium", 2);
-	glutAddMenuEntry("Difficult",3);
+	glutAddMenuEntry("Difficult", 3);
 
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 
 }
- void cgvInterface::menuHandle(int value)
+void cgvInterface::menuHandle(int value)
 {
 
-		switch (value) {
+	switch (value) {
 	case 1:
 		interface.scene.difficulty = 0.002;
 		break;
@@ -90,53 +87,34 @@ void cgvInterface::create_menu() {
 
 
 
-void cgvInterface::set_glutKeyboardFunc(unsigned char key, int x, int y) {
-  switch (key) {
+void cgvInterface::set_glutKeyboardFunc() {
 
-	 case 'w':
-		 interface.scene.movePlayer1(0.2);
-	 break;
-	 case 's':
-		 interface.scene.movePlayer1(-0.2);
-	break;
-	 case 'i':
-		 interface.scene.movePlayer2(0.2);
-		 break;
-	 case 'k':
-		 interface.scene.movePlayer2(-0.2);
-		 break;
-
-    case 'a': // enable/disable the visualization of the axes
-			interface.scene.set_axes(interface.scene.get_axes()?false:true);
-
-	  break;
-    case 27: // Escape key to exit
-      exit(1);
-    break;
-  }
-	glutPostRedisplay(); // renew the content of the window
+	if (GetAsyncKeyState(0x57)) interface.scene.movePlayer1(0.0025);
+	if (GetAsyncKeyState(0x49)) interface.scene.movePlayer2(0.0025);
+	if (GetAsyncKeyState(0x53)) interface.scene.movePlayer1(-0.0025);
+	if (GetAsyncKeyState(0x4B)) interface.scene.movePlayer2(-0.0025);
+	if (GetAsyncKeyState(0x41)) interface.scene.set_axes(interface.scene.get_axes() ? false : true);
+	if (GetAsyncKeyState(VK_ESCAPE)) exit(1);
 }
 
 void cgvInterface::set_glutReshapeFunc(int w, int h) {
-  // dimension of the viewport with a new width and a new height of the display window 
+	// dimension of the viewport with a new width and a new height of the display window 
 
+	// store the new values of the viewport and the display window. 
+	interface.set_width_window(w);
+	interface.set_height_window(h);
 
-  // store the new values of the viewport and the display window. 
-  interface.set_width_window(w);
-  interface.set_height_window(h);
-
-  // Set up the kind of projection to be used
-  interface.camera.apply(interface.camType);
+	// Set up the kind of projection to be used
+	interface.camera.apply(interface.camType);
 
 }
 
 void cgvInterface::set_glutDisplayFunc() {
-	GLuint impact_list[1024]; // array with the list of impacts when it is rendered in selection mode
 
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the window and the z-buffer
 
-	// set up the viewport
+														// set up the viewport
 	glViewport(0, 0, interface.get_width_window(), interface.get_height_window());
 
 	// Apply the camera and projection transformations according to its parameters and to the mode (selection or visualization)
@@ -149,9 +127,8 @@ void cgvInterface::set_glutDisplayFunc() {
 }
 
 void cgvInterface::init_callbacks() {
-	glutKeyboardFunc(set_glutKeyboardFunc);
 	glutReshapeFunc(set_glutReshapeFunc);
-	glutDisplayFunc(set_glutDisplayFunc); 
+	glutDisplayFunc(set_glutDisplayFunc);
 	glutVisibilityFunc(interface.visible);
 }
 
@@ -161,22 +138,12 @@ void cgvInterface::visible(int iVis)
 		glutIdleFunc(interface.idle);
 	else
 		glutIdleFunc(NULL);
-
 	return;
 }
 
 void cgvInterface::idle(void)
 {
-	float fTime, fSimTime;
-	float static fLastIdleTime = 0;
-
-	fTime = 1000 / 60;
-
-	fSimTime = fTime - fLastIdleTime;
-
+	set_glutKeyboardFunc();
 	interface.scene.ballMovement();
-
-	fLastIdleTime = fTime;
-
 	glutPostRedisplay();
 }
--- cgvScene3D.cpp
+++ cgvScene3D.cpp
@@ -1,14 +1,8 @@
 #include <cstdlib>
 #include <stdio.h>
-
-
+#include <sstream>
 
 #include "cgvScene3D.h"
-#include "cgvTexture.h"
-
-#pragma comment (lib,"opengl32.lib")
-#pragma comment (lib,"glu32.lib")
-#pragma comment (lib,"glut32.lib")	
 
 // Constructor methods -----------------------------------
 
@@ -16,12 +10,14 @@ GLfloat colour1[] = { 0,0.7,0 };
 GLfloat blue[] = { 0,0,1 };
 GLfloat red[] = { 1,0,0 };
 GLfloat white[] = { 1, 1, 1 };
-GLfloat black[] = { 0,0,0 };
 
 
-cgvScene3D::cgvScene3D () {
+cgvScene3D::cgvScene3D() {
 	axes = true;
-// Section B: initialize the attributes to control the degrees of freedom of the model
+	// Section B: initialize the attributes to control the degrees of freedom of the model
+
+	scoreP1 = 0;
+	scoreP2 = 0;
 
 	ballDirX = 1;
 	ballDirY = 0.0;
@@ -32,10 +28,9 @@ cgvScene3D::cgvScene3D () {
 	player1 = 0;
 	player2 = 0;
 
-	scoreP1 = 0;
-	scoreP2 = 0;
 	difficulty = 0.002;
-// Section D: initialize the attribute/s that identifies the select object and to colour it yellow
+
+	// Section D: initialize the attribute/s that identifies the select object and to colour it yellow
 
 }
 
@@ -45,33 +40,33 @@ cgvScene3D::~cgvScene3D() {}
 // Public methods ----------------------------------------
 
 void draw_axes(void) {
-  GLfloat red[]={1,0,0,1.0};
-  GLfloat green[]={0,1,0,1.0};
-  GLfloat blue[]={0,0,1,1.0};
+	GLfloat red[] = { 1,0,0,1.0 };
+	GLfloat green[] = { 0,1,0,1.0 };
+	GLfloat blue[] = { 0,0,1,1.0 };
 
 	glBegin(GL_LINES);
-    glMaterialfv(GL_FRONT,GL_EMISSION,red);
-		glVertex3f(1000,0,0);
-		glVertex3f(-1000,0,0);
+	glMaterialfv(GL_FRONT, GL_EMISSION, red);
+	glVertex3f(1000, 0, 0);
+	glVertex3f(-1000, 0, 0);
 
-    glMaterialfv(GL_FRONT,GL_EMISSION,green);
-		glVertex3f(0,1000,0);
-		glVertex3f(0,-1000,0);
+	glMaterialfv(GL_FRONT, GL_EMISSION, green);
+	glVertex3f(0, 1000, 0);
+	glVertex3f(0, -1000, 0);
 
-    glMaterialfv(GL_FRONT,GL_EMISSION,blue);
-		glVertex3f(0,0,1000);
-		glVertex3f(0,0,-1000);
+	glMaterialfv(GL_FRONT, GL_EMISSION, blue);
+	glVertex3f(0, 0, 1000);
+	glVertex3f(0, 0, -1000);
 	glEnd();
 }
 
-void cgvScene3D::drawPlayer1(){
+void cgvScene3D::drawPlayer1() {
 
 	glPushMatrix();
-		glMaterialfv(GL_FRONT, GL_EMISSION, red);
-		glTranslatef(0, 0, player1);
-		glTranslatef(4.5, 0, 0);
-		glScalef(0.25, 0.25, 2);
-		glutSolidCube(1);
+	glMaterialfv(GL_FRONT, GL_EMISSION, red);
+	glTranslatef(0, 0, player1);
+	glTranslatef(4.5, 0, 0);
+	glScalef(0.25, 0.25, 2);
+	glutSolidCube(1);
 	glPopMatrix();
 
 }
@@ -79,19 +74,19 @@ void cgvScene3D::drawPlayer1(){
 
 void cgvScene3D::drawPlayer2() {
 	glPushMatrix();
-		glMaterialfv(GL_FRONT, GL_EMISSION, blue);
-		glTranslatef(0, 0, player2);
-		glTranslatef(-4.5, 0, 0);
-		glScalef(0.25, 0.25, 2);
-		glutSolidCube(1);
+	glMaterialfv(GL_FRONT, GL_EMISSION, blue);
+	glTranslatef(0, 0, player2);
+	glTranslatef(-4.5, 0, 0);
+	glScalef(0.25, 0.25, 2);
+	glutSolidCube(1);
 	glPopMatrix();
 }
 
 void cgvScene3D::drawBall() {
 	glPushMatrix();
-		glMaterialfv(GL_FRONT, GL_EMISSION, white);
-		glTranslatef(ballX, 0, ballY);
-		glutSolidCube(0.25);
+	glMaterialfv(GL_FRONT, GL_EMISSION, white);
+	glTranslatef(ballX, 0, ballY);
+	glutSolidCube(0.25);
 	glPopMatrix();
 }
 
@@ -99,121 +94,60 @@ void cgvScene3D::drawBall() {
 void cgvScene3D::render(void) {
 
 	// lights
-	GLfloat light0[4]={5.0,5.0,5.0,1}; // point light source  
-	glLightfv(GL_LIGHT0,GL_POSITION,light0); // this light is placed here and it remains still 
+	GLfloat light0[4] = { 5.0,5.0,5.0,1 }; // point light source  
+	glLightfv(GL_LIGHT0, GL_POSITION, light0); // this light is placed here and it remains still 
 	glEnable(GL_LIGHT0);
-  
-  
+
+
 	// create the model
 	glPushMatrix(); // store the model matrices
 
-	  // draw the axes
-	  if (axes) draw_axes();
+					// draw the axes
+	if (axes) draw_axes();
 
- 		//glLightfv(GL_LIGHT0,GL_POSITION,light0); // the light is placed here and it moves with the scene
-	  float mesh_color[4] = {1.0, 0.0, 0.0, 1.0}; 
-	 // glMaterialfv(GL_FRONT,GL_EMISSION,mesh_color);
+	//glLightfv(GL_LIGHT0,GL_POSITION,light0); // the light is placed here and it moves with the scene
+	float mesh_color[4] = { 1.0, 0.0, 0.0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_EMISSION, mesh_color);
 
-	 // Top Border
-	  glPushMatrix();
-	    glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
-		glTranslatef(0, 0, 4);
-		glScalef(20, 0.5, 0.25);
-		glutSolidCube(1);
-      glPopMatrix();
+	//Top border
+	glPushMatrix();
+	glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
+	glTranslatef(0, 0, 4);
+	glScalef(20, 0.5, 0.25);
+	glutSolidCube(1);
+	glPopMatrix();
 
-	  //Bottom border
-	  glPushMatrix();
-	      glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
-		  glTranslatef(0, 0, -5);
-		  glScalef(20, 0.5, 0.5);
-		  glutSolidCube(1);
-	  glPopMatrix();
+	//Bottom border
+	glPushMatrix();
+	glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
+	glTranslatef(0, 0, -5);
+	glScalef(20, 0.5, 0.5);
+	glutSolidCube(1);
+	glPopMatrix();
 
-	  //Delimiter
-	  glPushMatrix();
-		  glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
-		  glTranslatef(0, 0, 5);
-		  glScalef(0.25, 0.5, 2);
-		  glutSolidCube(1);
+	//Delimiter
+	glPushMatrix();
+	 glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
+	 glTranslatef(0, 0, 5);
+	 glScalef(0.25, 0.5, 2);
+	 glutSolidCube(1);
 	 glPopMatrix();
 
-	 //glPushMatrix();
-	 //glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
-	 //drawRect(1, 1, 1, 1);
-	 //glPopMatrix();
+	//glPushMatrix();
 
+	 drawNumber(scoreP1, 1, 4.4);
+	 if (scoreP2 != 1) {
+		 drawNumber(scoreP2, -1.5, 4.4);
+	 }
+	 else {
+		 drawNumber(scoreP2, -1, 4.4);
+	 }
+	drawPlayer1();
+	drawPlayer2();
+	drawBall();
 
-	 // glPushMatrix();
-	 // glMaterialfv(GL_FRONT, GL_EMISSION, black);
-		////cgvTexture texture("map.bmp");
-		//  glBegin(GL_QUADS);
-		//  	glVertex3f(-5, 0.0, 4);
-		//	glTexCoord2f(-5, 4);
-		//  	glVertex3f(-5, 0.0, -5);
-		//	glTexCoord2f(-5, -5);
-		// 	glVertex3f(5, 0.0, -5);
-		//	glTexCoord2f(5, -5);
-		// 	glVertex3f(5, 0.0, 4);
-		//	glTexCoord2f(5, 4);
-		//  glEnd();
-	 // glPopMatrix();
+	glPopMatrix(); // restore the modelview matrix 
 
-	 // glPushMatrix();
-	 // glMaterialfv(GL_FRONT, GL_EMISSION, blue);
-	 //// cgvTexture texture("p1.bmp");
-	 // glBegin(GL_QUADS);
-		//  glVertex3f(-5, 0.0, 5);
-		//  glTexCoord2f(-5, 5);
-		//  glVertex3f(-5, 0.0, 4);
-		//  glTexCoord2f(-5, 4);
-		//  glVertex3f(-2, 0.0, 4);
-		//  glTexCoord2f(-2, 4);
-		//  glVertex3f(-2, 0.0, 5);
-		//  glTexCoord2f(-2, 5);
-		//  glEnd();
-	 // glPopMatrix();
-
-	 // glMaterialfv(GL_FRONT, GL_EMISSION, red);
-		//  //cgvTexture texture("map.bmp");
-		//  glBegin(GL_QUADS);
-		//  glVertex3f(5, 0.0, 5);
-		//  //glTexCoord2f(-5, 5);
-		//  glVertex3f(5, 0.0, 4);
-		//  //  glTexCoord2f(-5, 4.5);
-		//  glVertex3f(2, 0.0, 4);
-		//  // glTexCoord2f(5, 4.5);
-		//  glVertex3f(2, 0.0, 5);
-		//  //  glTexCoord2f(-4.5, 5);
-		//  glEnd();
-	 // glPopMatrix();
-
-	 //glPushMatrix();
-
-	 //glBegin(GL_QUADS);
-	 //glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
-	 ////Buttom Left
-	 //glVertex3f(0, 0, 0);
-	 ////Buttom Right
-	 //glVertex3f(0, 0, 0.1);
-	 ////Top Right
-	 //glVertex3f(0.5, 0, 0);
-
-	 //glVertex3f(0.5, 0, 0.1);
-
-	 //glEnd();
-
-	 drawNumber(3,1,4.4);
-	 drawNumber(3, -1, 4.4);
-
-
-
-	  drawPlayer1();
-	  drawPlayer2();
-	  drawBall();
-
-	glPopMatrix (); // restore the modelview matrix 
-  
 }
 
 
@@ -224,20 +158,16 @@ void cgvScene3D::movePlayer1(float value) {
 	if (value < 0 && player1 > -4) {
 		player1 += value;
 	}
-
 }
 
 
-void cgvScene3D::movePlayer2(float value) {	
-
+void cgvScene3D::movePlayer2(float value) {
 	if (value > 0 && player2 < 3) {
 		player2 += value;
 	}
 	if (value < 0 && player2 > -4) {
 		player2 += value;
 	}
-
-
 }
 
 void cgvScene3D::ballMovement() {
@@ -245,7 +175,7 @@ void cgvScene3D::ballMovement() {
 	ballY += ballDirY * difficulty;
 
 	float racket_width = 0.25;
-	float racket_height = 1.75;
+	float racket_height = 1.5;
 
 	// hit by left racket?
 	if (ballX < -4.5 + racket_width &&
@@ -254,7 +184,8 @@ void cgvScene3D::ballMovement() {
 		ballY > player2 - racket_height) {
 		// set fly direction depending on where it hit the racket
 		// (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
-		float t = ((ballY - player2) / (racket_height*2)) - 0.02;
+		float t = ((ballY - player2) / (racket_height)) - 0.02;
+
 		ballDirX = fabs(ballDirX); // force it to be positive
 		ballDirY = t;
 	}
@@ -266,7 +197,7 @@ void cgvScene3D::ballMovement() {
 		ballY > player1 - racket_height) {
 		// set fly direction depending on where it hit the racket
 		// (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
-		float t = ((ballY - player1) / (racket_height*2)) - 0.02;
+		float t = ((ballY - player1) / (racket_height * 2)) - 0.02;
 		ballDirX = -fabs(ballDirX); // force it to be negative
 		ballDirY = t;
 	}
@@ -280,7 +211,7 @@ void cgvScene3D::ballMovement() {
 		ballDirY = 0;
 	}
 
-	 // hit right wall?
+	// hit right wall?
 	if (ballX > 5) {
 		++scoreP2;
 		ballX = 0;
@@ -299,54 +230,9 @@ void cgvScene3D::ballMovement() {
 		ballDirY = 0.5; // force it to be positive
 	}
 
-
-
-
 }
 
-
-
-
-//void draw_quad(float div_x, float div_z,float _size_x,float _size_z) {
-//	float ini_x = 0.0;
-//	float ini_z = 0.0;
-//	float size_x = _size_x;
-//	float size_z = _size_z;
-//	float width = size_x / div_x;
-//	float length = size_z / div_z;
-//	float currentX = 0, currentZ = 0;
-//
-//
-//
-//	glNormal3f(0, 1, 0);
-//	glBegin(GL_QUADS);
-//	for (int i = 0; i < div_x; i++) {
-//		for (int j = 0; j < div_z; j++) {
-//			glTexCoord2f(currentX / size_x, currentZ / size_z);
-//			glVertex3f(currentX, 0.0, currentZ);
-//
-//			glTexCoord2f(currentX / size_x, (currentZ + length) / size_z);
-//			glVertex3f(currentX, 0.0, currentZ + length);
-//
-//			glTexCoord2f((currentX + width) / size_x, (currentZ + length) / size_z);
-//			glVertex3f(currentX + width, 0.0, currentZ + length);
-//
-//			glTexCoord2f((currentX + width) / size_x, currentZ / size_z);
-//			glVertex3f(currentX + width, 0.0, currentZ);
-//			currentZ += length;
-//		}
-//		currentX += width;
-//		currentZ = 0;
-//	}
-//	glEnd();
-//
-//
-//
-//
-//}
-
-
-void  cgvScene3D::drawNumber(int n,double x,double z) {
+void  cgvScene3D::drawNumber(int n, double x, double z) {
 
 	switch (n) {
 
@@ -358,31 +244,31 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 
 		glVertex3f(x, 0, z);
 
-		glVertex3f(x, 0, z+0.1);
+		glVertex3f(x, 0, z + 0.1);
 
-		glVertex3f(x+0.5, 0, z+0.1);
+		glVertex3f(x + 0.5, 0, z + 0.1);
 
-		glVertex3f(x+0.5, 0, z);
+		glVertex3f(x + 0.5, 0, z);
 
 		glEnd();
 		//--------
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x+0.5, 0, z+0.1);
+		glVertex3f(x + 0.5, 0, z + 0.1);
 
-		glVertex3f(x+0.5, 0, z+0.5);
+		glVertex3f(x + 0.5, 0, z + 0.5);
 
-		glVertex3f(x+0.4, 0, z+0.5);
+		glVertex3f(x + 0.4, 0, z + 0.5);
 
-		glVertex3f(x+0.4, 0, z+0.1);
+		glVertex3f(x + 0.4, 0, z + 0.1);
 
 		glEnd();
 		//-------
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x+0.4, 0, z+0.5);
+		glVertex3f(x + 0.4, 0, z + 0.5);
 
 		glVertex3f(x + 0, 0, z + 0.5);
 
@@ -400,9 +286,9 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 
 		glVertex3f(x, 0, z + 0.1);
 
-		glVertex3f(x+0.1, 0, z + 0.1);
+		glVertex3f(x + 0.1, 0, z + 0.1);
 
-		glVertex3f(x+0.1, 0, z + 0.5);
+		glVertex3f(x + 0.1, 0, z + 0.5);
 
 		glEnd();
 
@@ -418,11 +304,11 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 
 		glVertex3f(x, 0, z);
 
-		glVertex3f(x+0.1, 0, z );
+		glVertex3f(x + 0.1, 0, z);
 
 		glVertex3f(x + 0.1, 0, z + 0.5);
 
-		glVertex3f(x , 0, z + 0.5);
+		glVertex3f(x, 0, z + 0.5);
 
 		glEnd();
 
@@ -436,13 +322,13 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x, 0, z+0.15);
+		glVertex3f(x, 0, z + 0.15);
 
 		glVertex3f(x, 0, z + 0.25);
 
 		glVertex3f(x + 0.5, 0, z + 0.25);
 
-		glVertex3f(x + 0.5, 0, z+0.15);
+		glVertex3f(x + 0.5, 0, z + 0.15);
 
 		glEnd();
 		//-----
@@ -489,13 +375,13 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x, 0, z-0.15);
+		glVertex3f(x, 0, z - 0.15);
 
 		glVertex3f(x, 0, z - 0.05);
 
 		glVertex3f(x + 0.5, 0, z - 0.05);
 
-		glVertex3f(x + 0.5, 0, z-0.15);
+		glVertex3f(x + 0.5, 0, z - 0.15);
 
 		glEnd();
 
@@ -509,7 +395,7 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 
 		glVertex3f(x, 0, z - 0.1);
 
-		glVertex3f(x + 0.1, 0, z-0.1 );
+		glVertex3f(x + 0.1, 0, z - 0.1);
 
 		glVertex3f(x + 0.1, 0, z + 0.5);
 
@@ -520,13 +406,13 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x, 0, z+0.5);
+		glVertex3f(x, 0, z + 0.5);
 
 		glVertex3f(x, 0, z + 0.4);
 
 		glVertex3f(x + 0.5, 0, z + 0.4);
 
-		glVertex3f(x + 0.5, 0, z+0.5);
+		glVertex3f(x + 0.5, 0, z + 0.5);
 
 		glEnd();
 		//------
@@ -548,13 +434,13 @@ void  cgvScene3D::drawNumber(int n,double x,double z) {
 		glBegin(GL_QUADS);
 		glMaterialfv(GL_FRONT, GL_EMISSION, colour1);
 
-		glVertex3f(x, 0, z );
+		glVertex3f(x, 0, z);
 
 		glVertex3f(x, 0, z - 0.1);
 
-		glVertex3f(x + 0.5, 0, z -0.1);
+		glVertex3f(x + 0.5, 0, z - 0.1);
 
-		glVertex3f(x + 0.5, 0, z );
+		glVertex3f(x + 0.5, 0, z);
 
 		glEnd();
 		break;
